12e52f8dc1b7d2ba51c3a2af3b16b051163fe2e8
Jake Johnston <jakejohnston618@gmail.com>
2023-03-02 15:09:18 -0600

    talked about my dog

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myDogsName = "Tito";
+
+            string myFirstName = "Jake";
+
+            string myLastName = "Johnston";
+
+            int myDogsAge = 2;
+
+          char titosMiddleInitial = 'D';
+
+            double myDogsWeight = 85;
+
+            decimal dogFoodCost = 50.99m;
+
+            Console.WriteLine($"Hey, my names {myFirstName} {myLastName}, I've got a {myDogsAge} year old dog named {myDogsName} {titosMiddleInitial} {myLastName}, the D stands for dumb***.");
+
+            Console.WriteLine($"Ever since she was little we knew she'd be big, she weighs around {myDogsWeight} pounds, which doesn't help my wallet because her food is {dogFoodCost} dollars");
+
+            
+            
+
+
+
+
+
+
+
         }
     }
 }
